ef58c718065515d3e66417ee306ec727b6ef7f46
rewerze <gamermod801@gmail.com>
2025-03-19 13:30:38 +0100

    gyik elkezdve

--- client/src/components/FAQ.tsx
+++ client/src/components/FAQ.tsx
@@ -2,7 +2,50 @@ function FAQ() {
     return(
         <>
             <main className="nav-gap">
-
+                <h1 className="text-center p-2">
+                    Gyakori kérdések
+                </h1>
+                
+                <div className="mx-5">
+                    <h2>Milyen dokumentumokra van szükségem a bérléshez?</h2>
+                    <hr className="w-100" />
+                    <p>Ahhoz, hogy autót bérelhessen nálunk, szükséges, hogy legyen jogosítványa és meglegyen az elérhetősége. Minden egyéb követelényt <a href="/kovetelmenyek">itt</a> tud megtekinteni!</p>
+                </div>
+                <div className="mx-5 mt-5">
+                    <h2>Hogyan történik a bérlés folyamata?</h2>
+                    <hr className="w-100" />
+                    <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
+                </div>
+                <div className="mx-5 mt-5">
+                    <h2>Mi van, ha a bérlés ideje alatt autótörés történik?</h2>
+                    <hr className="w-100" />
+                    <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
+                </div>
+                <div className="mx-5 mt-5">
+                    <h2>Milyen módon tudom fizetni a bérlést?</h2>
+                    <hr className="w-100" />
+                    <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
+                </div>
+                <div className="mx-5 mt-5">
+                    <h2>Mik a bérlés időtartama és költségei?</h2>
+                    <hr className="w-100" />
+                    <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
+                </div>
+                <div className="mx-5 mt-5">
+                    <h2>Mi történik, ha késlekedem a visszavitelkor?</h2>
+                    <hr className="w-100" />
+                    <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
+                </div>
+                <div className="mx-5 mt-5">
+                    <h2>Hogyan történik az autó átvétele és visszaadása?</h2>
+                    <hr className="w-100" />
+                    <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
+                </div>
+                <div className="mx-5 mt-5">
+                    <h2>Bérelhetek autót más városban, mint ahol átvettem?</h2>
+                    <hr className="w-100" />
+                    <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
+                </div>
             </main>
         </>
     )
