c5e81796d1ed8eba813f3ccc79965098a44d8ea7
rewerze <gamermod801@gmail.com>
2025-03-26 11:20:50 +0100

    css fix, faq rework

--- client/src/components/FAQ.tsx
+++ client/src/components/FAQ.tsx
@@ -7,44 +7,68 @@ function FAQ() {
                 </h1>
                 
                 <div className="mx-5">
-                    <h2 className="display-5">Milyen dokumentumokra van szükségem a bérléshez?</h2>
+                    <details>
+                    <summary className="display-5">Milyen dokumentumokra van szükségem a bérléshez?
                     <hr className="w-100" />
+                    </summary>
                     <p>Ahhoz, hogy autót bérelhessen nálunk, szükséges, hogy legyen jogosítványa és meglegyen az elérhetősége. Minden egyéb követelényt <a href="/kovetelmenyek">itt</a> tud megtekinteni!</p>
+                    </details>
                 </div>
                 <div className="mx-5 mt-5">
-                    <h2 className="display-5">Hogyan történik a bérlés folyamata?</h2>
+                    <details>
+                        <summary className="display-5">Hogyan történik a bérlés folyamata?
+                            <hr className="w-100" />
+                        </summary>
+                    <p>A bérléshez elsősorban be kell jelentkeznie, majd a szükséges dokumentumok adja meg a profilján és ezután már szabadon bérelhet bármilyen autót amit talál nálunk!</p>
+                    </details>
+                </div>
+                <div className="mx-5 mt-5">
+                    <details>
+                        <summary className="display-5">Mi van, ha a bérlés ideje alatt autótörés történik?
                     <hr className="w-100" />
+                        </summary>
+                        <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
+                    </details>
+                </div>
+                <div className="mx-5 mt-5">
+                <details>
+                        <summary className="display-5">Milyen módon tudom fizetni a bérlést?
+                        <hr className="w-100" />
+                        </summary>
+                        <p>Online és helyszinen egyaránt ki tudod fizetni az autó árát kézpénzben vagy kártyával!</p>
+                    </details>
+                </div>
+                <div className="mx-5 mt-5">
+                <details>
+                        <summary className="display-5">Mik a bérlés időtartama és költségei?
+                        <hr className="w-100" />    
+                        </summary>
                     <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
+                    </details>
                 </div>
                 <div className="mx-5 mt-5">
-                    <h2 className="display-5">Mi van, ha a bérlés ideje alatt autótörés történik?</h2>
-                    <hr className="w-100" />
-                    <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
+                <details>
+                        <summary className="display-5">Mi történik, ha késlekedem a visszavitelkor?
+                        <hr className="w-100" />
+                        </summary>
+                        <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
+                    </details>
                 </div>
                 <div className="mx-5 mt-5">
-                    <h2 className="display-5">Milyen módon tudom fizetni a bérlést?</h2>
-                    <hr className="w-100" />
-                    <p>Online és helyszinen egyaránt ki tudod fizetni az autó árát kézpénzben vagy kártyával!</p>
+                <details>
+                        <summary className="display-5">Hogyan történik az autó átvétele és visszaadása?
+                        <hr className="w-100" />
+                        </summary>
+                        <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
+                    </details>
                 </div>
                 <div className="mx-5 mt-5">
-                    <h2 className="display-5">Mik a bérlés időtartama és költségei?</h2>
-                    <hr className="w-100" />
-                    <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
-                </div>
-                <div className="mx-5 mt-5">
-                    <h2 className="display-5">Mi történik, ha késlekedem a visszavitelkor?</h2>
-                    <hr className="w-100" />
-                    <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
-                </div>
-                <div className="mx-5 mt-5">
-                    <h2 className="display-5">Hogyan történik az autó átvétele és visszaadása?</h2>
-                    <hr className="w-100" />
-                    <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
-                </div>
-                <div className="mx-5 mt-5">
-                    <h2 className="display-5">Bérelhetek autót más városban, mint ahol átvettem?</h2>
-                    <hr className="w-100" />
-                    <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
+                <details>
+                        <summary className="display-5">Visszavihetem az autót másik depóba, mint ahol átvettem?
+                        <hr className="w-100" />
+                        </summary>
+                        <p>válasz válasz válasz válasz válasz válasz válasz válasz </p>
+                    </details>
                 </div>
             </main>
         </>
